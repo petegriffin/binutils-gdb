3b1853a301bea9fc35859732277af4d0eb3f2e8a
Kieran Bingham <kieran.bingham@linaro.org>
2015-12-04 20:22:26 +0000

    gdb/observer: Provide target_thread_changed observer

--- gdb/remote.c
+++ gdb/remote.c
@@ -2162,6 +2162,8 @@ static void
 set_general_thread (struct ptid ptid)
 {
   set_thread (ptid, 1);
+
+  observer_notify_target_thread_changed (ptid);
 }
 
 static void
@@ -7070,6 +7072,8 @@ remote_wait (struct target_ops *ops,
       if (!QUEUE_is_empty (stop_reply_p, stop_reply_queue))
 	mark_async_event_handler (remote_async_inferior_event_token);
     }
+
+  observer_notify_target_thread_changed (event_ptid);
 
   return event_ptid;
 }
