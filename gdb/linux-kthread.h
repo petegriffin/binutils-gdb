67a05f8eef80ddf1c9fffef58b01af51265ee63e
Kieran Bingham <kieran.bingham@linaro.org>
2016-03-17 10:51:21 +0000

    linux-kthread: Provide symbol, field, and offset helpers
    Integrate the helper functionality from LKD to find and cache the required
    symbols, addresses and offsets for extracting key information from the kernel

--- gdb/linux-kthread.h
+++ gdb/linux-kthread.h
@@ -20,6 +20,55 @@
 #ifndef LINUX_KTHREAD_H
 #define LINUX_KTHREAD_H 1
 
+#include "objfiles.h"
+
+struct addr_info
+{
+  char *name;
+  struct bound_minimal_symbol bmsym;
+  struct addr_info *next;
+};
+
+struct field_info
+{
+  char *struct_name;
+  char *field_name;
+  struct symbol *type;
+  int offset;
+  int size;
+  struct field_info *next;
+};
+
+int linux_init_addr (struct addr_info *field, int check);
+int linux_init_field (struct field_info *field, int check);
+
+static inline CORE_ADDR
+linux_get_address (struct addr_info *addr)
+{
+  if (addr->bmsym.minsym == NULL)
+    linux_init_addr (addr, 0);
+
+  return BMSYMBOL_VALUE_ADDRESS (addr->bmsym);
+}
+
+static inline unsigned int
+linux_get_field_offset (struct field_info *field)
+{
+  if (field->type == NULL)
+    linux_init_field (field, 0);
+
+  return field->offset;
+}
+
+static inline unsigned int
+linux_get_field_size (struct field_info *field)
+{
+  if (field->type == NULL)
+    linux_init_field (field, 0);
+
+  return field->size;
+}
+
 
 /* Set the function that supplies registers for an inactive thread for
    architecture GDBARCH to SUPPLY_KTHREAD.  */
